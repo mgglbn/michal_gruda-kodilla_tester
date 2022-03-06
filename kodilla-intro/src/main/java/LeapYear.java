6c28ec4a48ca129b5f62a92563d59611ae10a0ce
Michal G <mgglbn@gmail.com>
2022-03-06 21:29:31 +0100

    LeapYear2

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LeapYear {
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1900));
+        System.out.println(isLeapYear(1904));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2001));
+
+    }
+
+    private static boolean isLeapYear(int year){
+        if(year % 4 != 0){
+            return false;
+        } else if (year % 100 != 0){
+            return true;
+        } else if (year % 400 ==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
