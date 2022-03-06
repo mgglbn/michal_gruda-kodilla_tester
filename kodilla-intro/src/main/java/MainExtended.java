ee37c698068bdb6d0cdf0c097ea02ebd854c3b10
Michal G <mgglbn@gmail.com>
2022-03-06 19:51:17 +0100

    MainExtended2

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MainExtended {
+public class MainExtended {
+    public static void main(String[] args) {
+        boolean varBool = true;
+        String varStr= "String";
+        char varChar='X';
+        int varInt=1;
+        float varFloat=0.0f;
+        double varDouble=0.0001;
+
+        System.out.println(varBool);
+        System.out.println(varChar);
+        System.out.println(varDouble);
+        System.out.println(varFloat);
+        System.out.println(varInt);
+        System.out.println(varStr);
+
+
+
+    }
 }
