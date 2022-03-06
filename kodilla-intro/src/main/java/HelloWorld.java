287645888a7c27a0f73b4c6c3d7941cf51eaeaaf
Michal G <mgglbn@gmail.com>
2022-03-06 19:36:59 +0100

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        System.out.println("Michał");
+        System.out.println(37);
+        System.out.println('G');
+
+    }
+}
