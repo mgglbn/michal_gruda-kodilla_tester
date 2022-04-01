dbcf28f0a4b7bbf10d984fef78ca8763cfd1d951
Michal G <mgglbn@gmail.com>
2022-04-01 13:02:51 +0200

    week 5

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -6,27 +6,27 @@ import java.util.List;
 public class FlightRepository {
     private static List<Flight> flights = new ArrayList<>();
 
-    public static List<Flight> getFlightsTable(){
+    public static List<Flight> getFlightsTable() {
         return flights;
     }
 
-    public static List<Flight> getFlightsTable(String airport){
+    public static List<Flight> getFlightsTable(String airport) {
         List<Flight> flightsAtAirport = new ArrayList<>();
         for (Flight flight : flights) {
-            if(flight.getArrival().equals(airport) || flight.getDeparture().equals(airport)){
+            if (flight.getArrival().equals(airport) || flight.getDeparture().equals(airport)) {
                 flightsAtAirport.add(flight);
             }
         }
         return flightsAtAirport;
     }
 
-    public static void addFlight(Flight flight){
-        if (!flights.contains(flight)){
+    public static void addFlight(Flight flight) {
+        if (!flights.contains(flight)) {
             flights.add(flight);
         }
     }
 
-    public static int size(){
+    public static int size() {
         return flights.size();
     }
 }
