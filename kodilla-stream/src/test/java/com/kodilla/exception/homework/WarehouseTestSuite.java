160e14b861a0b854e57ef53e5ca624bc0f95070a
Michal G <mgglbn@gmail.com>
2022-04-06 22:05:34 +0200

    Homework 6.5

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseTestSuite {
+    @Test
+    public void testGetOrderWithException(){
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("1"));
+        warehouse.addOrder(new Order("2"));
+
+        assertThrows(OrderDoesNotExistException.class,()->warehouse.getOrder("3"));
+    }
+}
