540cf909277a14cf0a2ebd5f8276d0f9f92cf94f
Michal G <mgglbn@gmail.com>
2022-04-06 14:26:18 +0200

    Add filter

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -2,7 +2,11 @@ package com.kodilla.stream;
 
 public class UsersManager {
     public static void main(String[] args) {
-        UserRepository.getUserList().stream().map(UsersManager::getUserName).forEach(un-> System.out.println(un));
+        UserRepository.getUserList()
+                .stream()
+                .filter(u -> u.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .forEach(un-> System.out.println(un));
     }
     public static String getUserName(User user){
         return user.getUsername();
