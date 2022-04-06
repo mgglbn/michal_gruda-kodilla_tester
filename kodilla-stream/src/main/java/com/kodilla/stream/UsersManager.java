63064c513fa7f29f45e5325d3cda29659eb9eb0c
Michal G <mgglbn@gmail.com>
2022-04-06 14:14:42 +0200

    Change lambda to function reference

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -2,7 +2,7 @@ package com.kodilla.stream;
 
 public class UsersManager {
     public static void main(String[] args) {
-        UserRepository.getUserList().stream().map(u -> u.getUsername()).forEach(un-> System.out.println(un));
+        UserRepository.getUserList().stream().map(UsersManager::getUserName).forEach(un-> System.out.println(un));
     }
     public static String getUserName(User user){
         return user.getUsername();
