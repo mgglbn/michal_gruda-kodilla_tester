1204d03f110bb45ffd7c6678344ccca4231adfea
Michal G <mgglbn@gmail.com>
2022-04-06 14:07:31 +0200

    Stream first task

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,7 +1,10 @@
 package com.kodilla.stream;
 
-public class UserManager {
+public class UsersManager {
     public static void main(String[] args) {
         UserRepository.getUserList().stream().map(u -> u.getUsername()).forEach(un-> System.out.println(un));
+    }
+    public static String getUserName(User user){
+        return user.getUsername();
     }
 }
